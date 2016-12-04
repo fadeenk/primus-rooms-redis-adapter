8829ad2db31d0fa9bce7d588a5be9e9038d206e8
Fadee Kannah <fadeekannah@gmail.com>
2016-12-04 12:47:03 -0800

    Updated the tests and added tests for config function

--- test/primus-rooms-redis-adapter.spec.js
+++ test/primus-rooms-redis-adapter.spec.js
@@ -1,11 +1,13 @@
 'use strict';
 
 var Adapter = require('../');
+var Redis = require('ioredis');
 var expect = require('expect.js');
 
 describe('primus-rooms-adapter', function() {
+  var redis = new Redis();
   it('should have required methods', function() {
-    var adapter = new Adapter();
+    var adapter = new Adapter(redis);
     expect(adapter.set).to.be.a('function');
     expect(adapter.get).to.be.a('function');
     expect(adapter.del).to.be.a('function');
@@ -14,16 +16,34 @@ describe('primus-rooms-adapter', function() {
     expect(adapter.empty).to.be.a('function');
     expect(adapter.isEmpty).to.be.a('function');
     expect(adapter.clear).to.be.a('function');
+    expect(adapter.config).to.be.a('function');
     expect(adapter.wildcard).to.be.an('object');
   });
 
   it('initialized correctly', function() {
-    var adapter = new Adapter({metroplex: true});
+    var adapter = new Adapter(redis, {metroplex: true});
     expect(adapter.rooms).to.be(undefined);
     expect(adapter.sids).to.be(undefined);
     expect(adapter.namespace).to.be('bumblebee:');
     expect(adapter.omegaSupreme).to.be(false);
     expect(adapter.metroplex).to.be(true);
     expect(adapter.redis).to.be.an('object');
+  });
+
+  it('configure correctly', function() {
+    var adapter = new Adapter(redis);
+    expect(adapter.omegaSupreme).to.be(false);
+    expect(adapter.metroplex).to.be(false);
+    adapter.config({
+      omegaSupreme: true,
+      metroplex: true,
+      getClients: function() {
+        return Object.keys(primus.connections);
+      },
+    });
+    expect(adapter.omegaSupreme).to.be(true);
+    expect(adapter.metroplex).to.be(true);
+    expect(adapter.redis).to.be.an('object');
+    expect(adapter.getClients).to.be.a('function');
   });
 });
